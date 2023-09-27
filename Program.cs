0f6e6aa399b286bfc49b5e8d68f302c22a5d2d91
Esther Silva <141463785+sthellar@users.noreply.github.com>
2023-09-27 17:52:10 -0300

    Adicionar a atividade musica

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("Chandelier");
+Console.ResetColor();
+Console.WriteLine("_\n_");
+
+Console.WriteLine("Party girls don't get hurt");
+Console.WriteLine("Can't feel anything, when will I learn?");
+Console.WriteLine("I push it down, I push it down");
+Console.WriteLine("_\n_");
+
+Console.WriteLine("I'm the one for a good time call");
+Console.WriteLine("Phone's blowin' up, ringin' my doorbell");
+Console.WriteLine("I feel the love, feel the love");
+Console.WriteLine("_\n_");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("One, two, three, one, two, three, drink");
+Console.WriteLine("One, two, three, one, two, three, drink");
+Console.WriteLine("One, two, three, one, two, three, drink");
+Console.WriteLine("Throw 'em back 'til I lose count");
+Console.ResetColor();
+Console.WriteLine("_\n_");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("I'm gonna swing from the chandelier");
+Console.WriteLine("From the chandelier");
+Console.WriteLine("I'm gonna live like tomorrow doesn't exist");
+Console.WriteLine("Like it doesn't exist");
+Console.ResetColor();
+Console.WriteLine("_\n_");
+
+Console.WriteLine("I'm gonna fly like a bird through the night");
+Console.WriteLine("Feel my tears as they dry");
+Console.WriteLine("I'm gonna swing from the chandelier");
+Console.WriteLine("From the chandelier");
+Console.WriteLine("_\n_");
+
+Console.WriteLine("But I'm holding on for dear life");
+Console.WriteLine("Won't look down, won't open my eyes");
+Console.WriteLine("Keep my glass full until morning light");
+Console.WriteLine("'Cause I'm just holding on for tonight");
+Console.WriteLine("_\n_");
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("Help me, I'm holding on for dear life");
+Console.WriteLine("Won't look down, won't open my eyes");
+Console.WriteLine("Keep my glass full until morning light");
+Console.WriteLine("'Cause I'm just holding on for tonight");
+Console.WriteLine("On for tonight");
+Console.ResetColor();
+Console.WriteLine("_\n_");
+
+Console.WriteLine("Sun is up, I'm a mess");
+Console.WriteLine("Gotta get out now, gotta run from this");
+Console.WriteLine("Here comes the shame, here comes the shame ah");
+Console.WriteLine("_\n_");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("One, two, three, one, two, three, drink");
+Console.WriteLine("One, two, three, one, two, three, drink");
+Console.WriteLine("One, two, three, one, two, three, drink");
+Console.WriteLine("Throw 'em back 'til I lose count");
+Console.ResetColor();
+Console.WriteLine("_\n_");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("I'm gonna swing from the chandelier");
+Console.WriteLine("From the chandelier");
+Console.WriteLine("I'm gonna live like tomorrow doesn't exist");
+Console.WriteLine("Like it doesn't exist");
+Console.ResetColor();
+Console.WriteLine("_\n_");
+
+Console.WriteLine("I'm gonna fly like a bird through the night");
+Console.WriteLine("Feel my tears as they dry");
+Console.WriteLine("I'm gonna swing from the chandelier");
+Console.WriteLine("From the chandelier");
+Console.WriteLine("_\n_");
+
+Console.WriteLine("But I'm holding on for dear life");
+Console.WriteLine("Won't look down, won't open my eyes");
+Console.WriteLine("Keep my glass full until morning light");
+Console.WriteLine("'Cause I'm just holding on for tonight");
+Console.WriteLine("_\n_");
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("Help me, I'm holding on for dear life");
+Console.WriteLine("Won't look down, won't open my eyes");
+Console.WriteLine("Keep my glass full until morning light");
+Console.WriteLine("'Cause I'm just holding on for tonight");
+Console.WriteLine("On for tonight, on for tonight");
+Console.ResetColor();
+Console.WriteLine("_\n_");
+
+Console.WriteLine("'Cause I'm just holding on for tonight");
+Console.WriteLine("Oh, I'm just holding on for tonight");
+Console.WriteLine("On for tonight, on for tonight");
+Console.WriteLine("'Cause I'm just holding on for tonight");
+Console.WriteLine("'Cause I'm just holding on for tonight");
+Console.WriteLine("Oh, I'm just holding on for tonight");
+Console.WriteLine("On for tonight, on for tonight");
